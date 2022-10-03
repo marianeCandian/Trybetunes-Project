123188bb25141a635a38b072aee799dcdb2f2668
Mariane Candian <mariane.candian@gmail.com>
2022-10-03 18:38:12 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -11,7 +11,7 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
@@ -19,7 +19,7 @@ class App extends React.Component {
         <Route path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
         <Route path="*" component={ NotFound } />
-      </BrowserRouter>
+      </Switch>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  state = {
+    isDisabled: true,
+    user: '',
+    loading: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      user: value,
+    }, () => {
+      const { user } = this.state;
+      const lengthUser = user.length;
+      const valor = 3;
+      this.setState({ isDisabled: lengthUser < valor,
+      });
+    });
+  };
+
+  handleClick = async () => {
+    const { user } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name: user });
+    history.push('/search');
+  };
+
   render() {
+    const { isDisabled, user, loading } = this.state;
     return (
-      <h1 data-testid="page-login">Login</h1>
+      <div>
+        {loading
+          ? <Loading />
+          : (
+            <div data-testid="page-login">
+              <form>
+                <label htmlFor="name">
+                  Insira seu nome:
+                  <input
+                    type="text"
+                    data-testid="login-name-input"
+                    id="name"
+                    onChange={ this.handleChange }
+                    value={ user }
+                  />
+                </label>
+                <button
+                  type="button"
+                  disabled={ isDisabled }
+                  onClick={ this.handleClick }
+                  data-testid="login-submit-button"
+                >
+                  Entrar
+                </button>
+              </form>
+            </div>
+          )}
+      </div>
     );
   }
 }
+
+Login.propTypes = ({
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}).isRequired;
 
 export default Login;
