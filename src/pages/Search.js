a3c0a30bb883da4f7430ea971dbab4fe80c8de00
Mariane Candian <mariane.candian@gmail.com>
2022-10-03 20:52:33 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    isDesabled: true,
+    cantor: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      cantor: value,
+    }, () => {
+      const { cantor } = this.state;
+      const lengthCantor = cantor.length;
+      const valor = 2;
+      this.setState({ isDesabled: lengthCantor < valor,
+      });
+    });
+  };
+
   render() {
+    const { isDesabled, cantor } = this.state;
     return (
       <div>
         <Header />
-        <h4 data-testid="page-search">Search</h4>
+        <div data-testid="page-search">
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+              value={ cantor }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isDesabled }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
