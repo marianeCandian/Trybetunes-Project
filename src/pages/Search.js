96216a4a490539c491d6ee0a33ba59b6d60a094a
Mariane Candian <mariane.candian@gmail.com>
2022-10-04 15:56:26 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loadin from './Loading';
 
 class Search extends React.Component {
   state = {
     isDesabled: true,
     cantor: '',
+    loading: false,
+    albuns: [],
+    cantorRecuperado: '',
   };
 
   handleChange = ({ target }) => {
@@ -20,28 +26,63 @@ class Search extends React.Component {
     });
   };
 
+  handleClick = async () => {
+    this.setState({ loading: true });
+    const { cantor } = this.state;
+    const album = await searchAlbumsAPI(cantor);
+    this.setState({
+      cantorRecuperado: cantor,
+      loading: false,
+      albuns: album,
+      cantor: '',
+    });
+  };
+
   render() {
-    const { isDesabled, cantor } = this.state;
+    const { isDesabled, cantor, loading, albuns, cantorRecuperado } = this.state;
     return (
       <div>
         <Header />
-        <div data-testid="page-search">
-          <form>
-            <input
-              type="text"
-              data-testid="search-artist-input"
-              onChange={ this.handleChange }
-              value={ cantor }
-            />
-            <button
-              type="button"
-              data-testid="search-artist-button"
-              disabled={ isDesabled }
-            >
-              Pesquisar
-            </button>
-          </form>
-        </div>
+        { loading
+          ? <Loadin />
+          : (
+            <div data-testid="page-search">
+              <form>
+                <input
+                  type="text"
+                  data-testid="search-artist-input"
+                  onChange={ this.handleChange }
+                  value={ cantor }
+                />
+                <button
+                  type="button"
+                  data-testid="search-artist-button"
+                  disabled={ isDesabled }
+                  onClick={ this.handleClick }
+                >
+                  Pesquisar
+                </button>
+              </form>
+              <div>
+                {
+                  albuns.length > 0
+                    ? <p>{`Resultado de álbuns de: ${cantorRecuperado}`}</p>
+                    : <p>Nenhum álbum foi encontrado</p>
+                }
+                { albuns.map((album) => (
+                  <div key={ album.collectionId }>
+                    <img src={ album.artworkUrl100 } alt={ album.colletionName } />
+                    <h2>{ album.collectionName }</h2>
+                    <p>{ album.artistaName }</p>
+                    <Link
+                      to={ `/album/${album.collectionId}` }
+                      data-testid={ `link-to-album-${album.collectionId}` }
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
       </div>
     );
   }
